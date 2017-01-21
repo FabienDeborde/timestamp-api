8afc1497f61e7010ea4c56b0f652d01a4f464e67
Fabien Deborde <fabien.webdev@gmail.com>
2017-01-21 07:55:36 +0000

    Finished the challenge

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
 var express = require("express");
 
+var app = express();
 
-var app = express()
+app.get('*', function (req, res) {
+  var date = req.path.substring(1); // Cut out the 1st /
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; // Months list
+  // The date object containing both formats to return
+  var dateObj = {
+    'unix': 0,
+    'natural': ''
+  };
+  // RegEx to search for 'natural' format, naturalResult will output an object if it's a match, if not it returns null
+  var naturalRegEx = /^([a-zA-Z]*)%20(\d{2}),%20(\d{4})$/;
+  var naturalResult = date.match(naturalRegEx);
+  // RegEx to search for unix format, unixResult will output an object if it's a match, if not it returns null
+  var unixRegex = /^\d*$/;
+  var unixResult = date.match(unixRegex);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  if (naturalResult) {  // Check if it's a 'natural' date format
+    // Get the date info
+    var year = naturalResult[3];
+    var month = naturalResult[1];
+    var day = naturalResult[2];
+    // Format the date and insert the values inside the dateObj object
+    dateObj.natural = month + ' ' + day + ', ' + year;
+    dateObj.unix = Date.parse(year, month, day);
+  } else if (unixResult) {  // Check if it's a unix date format
+    // Get the date info
+    var d = parseInt(date, 10); // Change the date from String to Number
+    var unixDate = new Date(d *1000); // Create new date from the unix timestamp (took me a while to understand this timestamp was not in milliseconds)
+    var year = unixDate.getFullYear();
+    var month = months[unixDate.getMonth()];
+    var day = unixDate.getDate();
+    // Format the date and insert the values inside the dateObj object
+    dateObj.unix = d;
+    dateObj.natural = month + ' ' + day + ', ' + year;
+  } else {   // If it's neither a 'natural' nor a unix date format
+    // Ouput an error message
+    console.error('Wrong date format');
+  }
+  // res.end can't output an object so we need to stringify it
+  res.end(JSON.stringify(dateObj));
+});
+
+
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+  console.log('Timestamp app listening on port 8080!');
+});
